c57340d07a8f9a7bd8366ed97d3eb1a85a775e7a
James B <jhbowen047@gmail.com>
2015-01-01 19:34:49 -0600

    Basic HTML Hard-coded Grid

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+(function() {
+	'use strict';
+
+	var gulp = require('gulp'),
+	nodemon = require('gulp-nodemon'),
+	watch = require('gulp-watch'),
+	jshint = require('gulp-jshint'),
+	livereload = require('gulp-livereload'),
+	_paths = ['server/**/*.js', 'client/js/*.js'];
+	
+	gulp.task('nodemon', function() {
+		nodemon({
+			script: 'server/app.js',
+			env: {
+				'NODE_ENV': 'development'
+			}
+		}).on('restart');
+	});
+
+	gulp.task('watch', function() {
+		livereload.listen();
+
+		gulp.src(_paths, {
+			read: false
+		}).pipe(watch('all'
+		)).pipe(jshint())
+		.pipe(jshint.reporter('default'));
+
+		gulp.watch(_paths).on('change', livereload.changed);
+	});
+
+	gulp.task('lint', function() {
+		gulp.src(_paths).pipe(jshint()).pipe(jshint.reporter('default'));
+	});
+
+	gulp.task('default', ['lint', 'nodemon', 'watch']);
+}());
+
---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+(function() {
+	'use strict';
+	var express = require('express');
+	var router = express.Router();
+
+	router.get('/', function(req,res) {
+		res.render('index.html');
+	});
+
+	module.exports = router;
+
+}());
